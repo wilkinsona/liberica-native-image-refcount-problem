5245540193aa07f5c5c0a124f580d9380f5fc437
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-02-17 09:54:14 +0000

    Add repro for refcount has gone to zero problem

---
+++ src/main/java/com/example/jdbc/mysql/CLR.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.jdbc.mysql;
+
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CLR implements CommandLineRunner {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public CLR(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public void run(String... args) {
+		insertAuthors();
+		queryAuthors();
+	}
+
+	private void queryAuthors() {
+		List<Author> authors = jdbcTemplate.query("SELECT * FROM authors", Author.Mapper.INSTANCE);
+		for (Author author : authors) {
+			System.out.printf("Found author: %s%n", author);
+		}
+	}
+
+	private void insertAuthors() {
+		List<String> authors = List.of("mbhave", "snicoll", "wilkinsona");
+
+		for (String author : authors) {
+			this.jdbcTemplate.update("INSERT INTO authors (name) VALUES (?)", author);
+		}
+	}
+
+}
